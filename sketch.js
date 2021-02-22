f8bfe8af2d7d4ecf020070366325af8f311a32e7
DurgaSMenon <72439481+DurgaSMenon@users.noreply.github.com>
2021-02-22 10:36:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(windowWidth/2,windowHeight/1.5);
+    engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	let canvasmouse = Mouse.create(canvas.elt);
+	canvasmouse.pixelRatio = pixelDensity();
+	let options = {
+		mouse:canvasmouse
+	};
+
+	mConstraint = MouseConstraint.create(engine,options);
+	World.add(world,mConstraint);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+function mouseDragged(){
+	Matter.Body.setPosition(pendulum.body,{x:mouseX, y:mouseY});
+}
+
+
+
